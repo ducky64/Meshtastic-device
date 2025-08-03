36e4683462d766512de94b0729ab58156bfa166b
Richard Lin <elpato25@gmail.com>
2025-08-03 12:54:19 -0700

    working board

--- variants/esp32-s3-pico/pins_arduino.h
+++ variants/esp32-s3-pico/pins_arduino.h
@@ -7,22 +7,20 @@
 #define USB_PID 0x1001
 
 // The default Wire will be mapped to PMU and RTC
-static const uint8_t SDA = 15;
-static const uint8_t SCL = 16;
+static const uint8_t SDA = 18;
+static const uint8_t SCL = 17;
 
 // Default SPI will be mapped to Radio
-static const uint8_t MISO = 37;
-static const uint8_t SCK = 35;
-static const uint8_t MOSI = 36;
-static const uint8_t SS = 14;
+static const uint8_t SS = 7;
+static const uint8_t MOSI = 6;
+static const uint8_t MISO = 3;
+static const uint8_t SCK = 5;
 
-static const uint8_t BAT_ADC_PIN = 26;
+#define SPI_MOSI (11)
+#define SPI_SCK (14)
+#define SPI_MISO (2)
+#define SPI_CS (13)
 
-// #define SPI_MOSI                    (11)
-// #define SPI_SCK                     (14)
-// #define SPI_MISO                    (2)
-// #define SPI_CS                      (13)
-
-// #define SDCARD_CS                   SPI_CS
+#define SDCARD_CS SPI_CS
 
 #endif /* Pins_Arduino_h */
